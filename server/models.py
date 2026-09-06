f303e6a0ef42a21514cb7d3fd769f4733933c580
ZizaKariuki <zizawabetti@gmail.com>
2026-09-06 20:15:07 +0300

    Defining Workout-exercises model

--- server/models.py
+++ server/models.py
@@ -24,3 +24,16 @@ class Workout(db.Model):
 
     def __repr__(self):
         return f'<Workout {self.id}: {self.date}>'
+
+class WorkoutExercise(db.Model):
+    __tablename__ = 'workout_exercises'
+
+    id = db.Column(db.Integer, primary_key=True)
+    workout_id = db.Column(db.Integer, db.ForeignKey('workouts.id'))
+    exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.id'))
+    reps = db.Column(db.Integer)
+    sets = db.Column(db.Integer)
+    duration_seconds = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<WorkoutExercise {self.id}>'
