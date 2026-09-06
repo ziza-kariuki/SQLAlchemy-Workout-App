1a30c404501129c372fe733c7f9191db0b899b56
ZizaKariuki <zizawabetti@gmail.com>
2026-09-06 20:13:33 +0300

    Defining exercise model

--- server/models.py
+++ server/models.py
@@ -3,3 +3,13 @@ from sqlalchemy.orm import validates
 db = SQLAlchemy()
 
 # Define Models here
+class Exercise(db.Model):
+    __tablename__ = 'exercises'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    category = db.Column(db.String)
+    equipment_needed = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return f'<Exercise {self.id}: {self.name}>'
