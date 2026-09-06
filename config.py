b572328d98f805eca0e1df5771e80a8ff8c3c917
ZizaKariuki <zizawabetti@gmail.com>
2026-09-07 00:22:01 +0300

    Initializing Database and Migrate

---
+++ config.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy()
+db.init_app(app)
+migrate = Migrate(app, db)
